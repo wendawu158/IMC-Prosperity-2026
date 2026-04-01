5cc1b51358c759c2faad9dda8a22a82b7431d611
Wenda Wu <wendawu3011@gmail.com>
2026-04-01 17:13:00 +0100

    Add more log files Signed-off-by: Wenda Wu <wendawu3011@gmail.com>

---
+++ Logs/10-Tutorial-Round/38852.py
@@ -0,0 +1,108 @@
+from datamodel import *
+
+
+class Trader:
+
+    def bid(self):
+        pass
+
+    def run(self, state: TradingState):
+        """
+        Overarching algorithm
+        """
+
+        # Capture to a variable
+        self.state = state
+
+        # Output variables initialising
+        self.orders: Dict[Symbol, List[Order]] = dict()
+        self.traderData = ""
+
+        self.emerald()
+
+        return self.orders, 0, self.traderData
+
+    def emerald(self):
+        """
+        Trading algorithm specifically for Emeralds
+        """
+
+        # Emeralds
+        EMERALDS = "EMERALDS"
+
+        # True price for Emeralds
+        EMERALDS_TRUE_PRICE = 10000
+
+        # The order book without us
+        EMERALDS_STABLE_ASK = 10008
+        EMERALDS_STABLE_BID = 9992
+
+        # Useful information from the TradingState
+        self.orders[EMERALDS] = list()
+        bids_emerald = self.state.order_depths[EMERALDS].buy_orders
+        asks_emerald = self.state.order_depths[EMERALDS].sell_orders
+
+        # Market making
+        best_ask = EMERALDS_STABLE_ASK
+        best_bid = EMERALDS_STABLE_BID
+
+        # How many emeralds are we holding?
+        try:
+            position_emerald = self.state.position[EMERALDS]
+        except KeyError:
+            position_emerald = 0
+
+        # Check for immediately profitable trades
+        # We also want to take fair trades, so that the bots can't take them and must trade with us
+        if len(bids_emerald.keys()) != 0:
+            for bid in bids_emerald.keys():
+                if bid > EMERALDS_TRUE_PRICE:
+                    self.orders[EMERALDS].append(
+                        Order(EMERALDS, bid, -1 * bids_emerald[bid])
+                    )   # Here we are placing a sell to any bid above the fair price
+                if bid > best_bid:
+                    best_bid = bid
+
+        if len(asks_emerald.keys()) != 0:
+            for ask in asks_emerald.keys():
+                if ask < EMERALDS_TRUE_PRICE:
+                    self.orders[EMERALDS].append(
+                        Order(EMERALDS, ask, -1 * asks_emerald[ask])
+                    )   # Here we are placing a buy to any ask below the fair price
+                if ask < best_ask:
+                    best_ask = ask
+
+        # Undercutting the competition, but still need to be above the true price
+        if best_ask > EMERALDS_TRUE_PRICE:
+            best_ask -= 1
+        else:
+            best_ask = EMERALDS_TRUE_PRICE
+
+        if best_bid < EMERALDS_TRUE_PRICE:
+            best_bid += 1
+        else:
+            best_bid = EMERALDS_TRUE_PRICE
+
+        self.orders[EMERALDS].append(
+            Order(EMERALDS, best_bid, 40)
+        )
+        self.orders[EMERALDS].append(
+            Order(EMERALDS, best_ask, -40)
+        )
+
+        # Rebalance our inventory, if we are out of balance
+        if position_emerald > 0 and bids_emerald[EMERALDS_TRUE_PRICE]:
+
+            self.orders[EMERALDS].append(
+                Order(EMERALDS, EMERALDS_TRUE_PRICE,
+                      -1 * min(position_emerald, bids_emerald[EMERALDS_TRUE_PRICE])
+                      )  # We don't wanna over correct
+            )
+
+        elif position_emerald < 0 and asks_emerald[EMERALDS_TRUE_PRICE]:
+
+            self.orders[EMERALDS].append(
+                Order(EMERALDS, EMERALDS_TRUE_PRICE,
+                      -1 * max(position_emerald, asks_emerald[EMERALDS_TRUE_PRICE])
+                      )  # We don't wanna over correct
+            )
