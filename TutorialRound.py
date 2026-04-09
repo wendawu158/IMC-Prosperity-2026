a909abd12b6b9bc586f3d6842fd6b738a2763348
ama1125 <ama1125@ic.ac.uk>
2026-04-09 21:00:06 +0100

    tomato trading fuction update

--- TutorialRound.py
+++ TutorialRound.py
@@ -143,36 +143,118 @@ class Trader:
         Trading algorithm specifically for tomatoes
         """
 
-        # Tomatoes
-        TOMATOES = "TOMATOES"
+        #Trading algorithm specifically for Emeralds
+
+        #Ticker
+        """"
+        TOMATO are a stable product
+        Their true price stays at midpoint exactly
+        The bid/ask given by the bot market makers is always 
+        However, sometimes we can see traders taking exit liquidity
+
+        Strategy
+        Undercut other market makers by 1 unit, offering at just above and below their bid/ask
+        Use people taking exit liquidity to rebalance position
+        Never take bids/asks below/above 10000
+        """""
+        
+
+        # Useful information from the TradingState
+        self.orders[TOMATO] = list()
+        bids_tomato = self.state.order_depths[TOMATO].buy_orders
+        asks_tomato = self.state.order_depths[TOMATO].sell_orders
+
+        # TOMATO
+        TOMATO = "Tomato"
+
+        # True price for TOMATO
+        TOMATO_TRUE_PRICE =  
+
+        # The order book without us
+        TOMATO_STABLE_ASK = 
+        TOMATO_STABLE_BID = 
 
         # Market making
-        self.orders[TOMATOES] = list()
-        best_ask = sorted(self.state.order_depths[TOMATOES].sell_orders.keys())[0]
-        best_bid = sorted(self.state.order_depths[TOMATOES].buy_orders.keys())[-1]
+        best_ask = TOMATO_STABLE_ASK
+        best_bid = TOMATO_STABLE_BID
 
-        position_tomatoes = 0
+        # The number of emeralds that we hold
+        position_tomato = 0
 
+        # How many TOMATOs are we holding?
         try:
-            position_tomatoes= self.state.position[TOMATOES]
+            position_tomato = self.state.position[TOMATO]
         except KeyError:
             pass
 
         # Check for immediately profitable bids
-        if len(bids_emerald.keys()) != 0:
+        if len(bids_tomato.keys()) != 0:
 
             # Checking all the bids
-            for bid in bids_emerald.keys():
+            for bid in bids_tomato.keys():
 
                 # Is there a bid better than the stable price of the EMERALDS?
-                if bid > EMERALDS_TRUE_PRICE:
-                    self.orders[EMERALDS].append(
-                        Order(EMERALDS, bid, -1 * bids_emerald[bid])
+                if bid > TOMATO_TRUE_PRICE:
+                    self.orders[TOMATO].append(
+                        Order(TOMATO, bid, -1 * bids_tomato[bid])
                     )   # Here we are placing a sell to any bid above the fair price
 
                 # Figuring out the best bid
                 if bid > best_bid:
                     best_bid = bid
+
+        # Check for immediately profitable asks
+        if len(asks_tomato.keys()) != 0:
+
+            # Checking all the asks
+            for ask in asks_tomato.keys():
+
+                # Is there an ask better than the stable price of the EMERALDS?
+                if ask < TOMATO_TRUE_PRICE:
+                    self.orders[TOMATO].append(
+                        Order(TOMATO, ask, -1 * asks_tomato[ask])
+                    )   # Here we are placing a buy to any ask below the fair price
+
+                # Figuring out the best ask
+                if ask < best_ask:
+                    best_ask = ask
+
+        # Undercutting the competition, but still need to be above the true price
+        # Minimum price change is one unit of currency, hence the +/- 1
+        if best_ask > TOMATO_TRUE_PRICE:
+            best_ask -= 1
+        else:
+            best_ask = TOMATO_TRUE_PRICE
+
+        if best_bid < TOMATO_TRUE_PRICE:
+            best_bid += 1
+        else:
+            best_bid = TOMATO_TRUE_PRICE
+
+        # Adding our normal orders
+        # We aim to make profit on these trades
+        self.orders[TOMATO].append(
+            Order(TOMATO, best_bid, 40)
+        )
+        self.orders[TOMATO].append(
+            Order(TOMATO, best_ask, -40)
+        )
+
+        # Rebalance our inventory, if we are out of balance
+        # This is to prevent us from getting into situations where we are unable to place profitable trades
+        # Basically, we buy up any trades at the true price to rebalance our position
+        if position_tomato > 20 and bids_emerald.get(TOMATO_TRUE_PRICE, 0) > 0:
+            self.orders[TOMATO].append(
+                Order(TOMATO, TOMATO_TRUE_PRICE,
+                      -1 * bids_tomato.get(TOMATO_TRUE_PRICE)
+                      )  # We don't wanna over correct
+            )
+
+        elif position_tomato < 20 and asks_tomato.get(TOMATO_TRUE_PRICE, 0) > 0:
+            self.orders[TOMATO].append(
+                Order(TOMATO, TOMATO_TRUE_PRICE,
+                      -1 * asks_tomato.get(TOMATO_TRUE_PRICE)
+                      )  # We don't wanna over correct
 
 
 
